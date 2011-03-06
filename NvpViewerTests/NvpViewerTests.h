29ccc21a4a67ddf78074576d7c626390714b06d9
Guigui Gaga <guiguigaga@Guigui-Gagas-Mac-Pro.local>
2011-03-06 17:12:57 +0000

    Initial Commit

---
+++ NvpViewerTests/NvpViewerTests.h
@@ -0,0 +1,17 @@
+//
+//  NvpViewerTests.h
+//  NvpViewerTests
+//
+//  Created by Guigui Gaga on 3/6/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface NvpViewerTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ NvpViewerTests/NvpViewerTests.m
@@ -0,0 +1,33 @@
+//
+//  NvpViewerTests.m
+//  NvpViewerTests
+//
+//  Created by Guigui Gaga on 3/6/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "NvpViewerTests.h"
+
+
+@implementation NvpViewerTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in NvpViewerTests");
+}
+
+@end
